5d21c573cae09ec4ba0da9e533dbc423960491f8
Chris Harris <charris1@companieshouse.gov.uk>
2023-06-15 13:14:17 +0100

    removed outputs resource

--- groups/chips-training7/outputs.tf
+++ groups/chips-training7/outputs.tf
@@ -2,6 +2,6 @@ output "db_dns_names" {
   value = aws_route53_record.db_dns.*.name
 }
 
-resource "vault_generic_secret" "chips-training7-outputs" {
-  path = "applications/${var.aws_profile}/chips-training7/${var.application}/outputs"
-}
+# resource "vault_generic_secret" "chips-training7-outputs" {
+#   path = "applications/${var.aws_profile}/chips-training7/${var.application}/outputs"
+# }
