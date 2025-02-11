ea050e5815de1d56cbd57f628e00d3b1144b3b04
charris-CH <charris1@companieshouse.gov.uk>
2025-02-11 13:00:38 +0000

    updated dns names

--- groups/chips-training/dns.tf
+++ groups/chips-training/dns.tf
@@ -2,7 +2,7 @@ resource "aws_route53_record" "chips_training" {
   count = var.instance_count
 
   zone_id = data.aws_route53_zone.private_zone.zone_id
-  name    = "${var.service_subtype}-${count.index + 1}"
+  name    = format("%s-%02d", var.service_subtype, count.index + 1)
   type    = "A"
   ttl     = 300
   records = [aws_instance.chips_training[count.index].private_ip]
