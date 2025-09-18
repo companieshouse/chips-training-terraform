f69394d9d8439504df27961bc9658499157f2413
charris-CH <charris1@companieshouse.gov.uk>
2025-09-18 09:33:03 +0100

    updated alarms

--- groups/chips-training-db-01/cloudwatch.tf
+++ groups/chips-training-db-01/cloudwatch.tf
@@ -1,24 +1,9 @@
-resource "aws_cloudwatch_metric_alarm" "chips_training_db_01_server_cpu95" {
-  alarm_name                = "WARNING-chips-training-01-CPUUtilization"
-  evaluation_periods        = "1"
-  comparison_operator       = "GreaterThanOrEqualToThreshold"
-  metric_name               = "CPUUtilization"
-  namespace                 = "CHIPS-TRAINING-01/EC2"
-  period                    = "60"
-  statistic                 = "Maximum"
-  threshold                 = "95"
-  alarm_description         = "This metric monitors ec2 cpu utilization system"
-  insufficient_data_actions = []
-  alarm_actions             = [aws_sns_topic.chips_training_db_01.arn]
-  ok_actions                = [aws_sns_topic.chips_training_db_01.arn]
-}
-
-resource "aws_cloudwatch_metric_alarm" "chips_training_db_01_server_StatusCheckFailed" {
-  alarm_name                = "CRITICAL-chips-training-01-StatusCheckFailed"
+resource "aws_cloudwatch_metric_alarm" "chips_training_db_01_StatusCheckFailed" {
+  alarm_name                = "${upper(var.environment)} - CRITICAL - chips-training-01-StatusCheckFailed"
   evaluation_periods        = "1"
   comparison_operator       = "GreaterThanOrEqualToThreshold"
   metric_name               = "StatusCheckFailed"
-  namespace                 = "CHIPS-TRAINING-01/EC2"
+  namespace                 = "AWS/EC2"
   period                    = "300"
   statistic                 = "Maximum"
   threshold                 = "1"
@@ -26,38 +11,179 @@ resource "aws_cloudwatch_metric_alarm" "chips_training_db_01_server_StatusCheckF
   insufficient_data_actions = []
   alarm_actions             = [aws_sns_topic.chips_training_db_01.arn]
   ok_actions                = [aws_sns_topic.chips_training_db_01.arn]
+  dimensions = {
+    instance_id = aws_instance.chips_training_db_01[0].id
+  }
+}
+# --------------------------------------------------------------------------------------
+resource "aws_cloudwatch_metric_alarm" "chips_training_db_01_cpu95" {
+  alarm_name                = "${upper(var.environment)} - CRITICAL - chips-training-01-CPUUtilization"
+  evaluation_periods        = "1"
+  comparison_operator       = "GreaterThanOrEqualToThreshold"
+  metric_name               = "CPUUtilization"
+  namespace                 = "AWS/EC2"
+  period                    = "300"
+  statistic                 = "Maximum"
+  threshold                 = "95"
+  alarm_description         = "This metric monitors ec2 cpu utilization system"
+  insufficient_data_actions = []
+  alarm_actions             = [aws_sns_topic.chips_training_db_01.arn]
+  ok_actions                = [aws_sns_topic.chips_training_db_01.arn]
+  dimensions = {
+    instance_id = aws_instance.chips_training_db_01[0].id
+  }
 }
 
-resource "aws_cloudwatch_metric_alarm" "chips_training_db_01_server_disk_space" {
-  alarm_name          = "CRITICAL-chips-training-01-disk-space"
+resource "aws_cloudwatch_metric_alarm" "chips_training_db_01_cpu75" {
+  alarm_name                = "${upper(var.environment)} - WARNING - chips-training-01-CPUUtilization"
+  evaluation_periods        = "1"
+  comparison_operator       = "GreaterThanOrEqualToThreshold"
+  metric_name               = "CPUUtilization"
+  namespace                 = "AWS/EC2"
+  period                    = "300"
+  statistic                 = "Maximum"
+  threshold                 = "75"
+  alarm_description         = "This metric monitors ec2 cpu utilization system"
+  insufficient_data_actions = []
+  alarm_actions             = [aws_sns_topic.chips_training_db_01.arn]
+  ok_actions                = [aws_sns_topic.chips_training_db_01.arn]
+  dimensions = {
+    instance_id = aws_instance.chips_training_db_01[0].id
+  }
+}
+
+# --------------------------------------------------------------------------------------
+resource "aws_cloudwatch_metric_alarm" "chips_training_db_01_root_disk_space_crit" {
+  alarm_name          = "${upper(var.environment)} - CRITICAL - chips-training-01-root-disk-space"
   comparison_operator = "GreaterThanOrEqualToThreshold"
   metric_name         = "disk_used_percent"
-  namespace           = "CHIPS-TRAINING-01/EC2"
+  namespace           = "CWAgent"
+  period              = "300"
+  evaluation_periods  = "1"
+  statistic           = "Average"
+  threshold           = "90"
+  alarm_description   = "The disk space average precentage is over 80% for the last 10 minutes"
+  alarm_actions       = [aws_sns_topic.chips_training_db_01.arn]
+  ok_actions          = [aws_sns_topic.chips_training_db_01.arn]
+  dimensions = {
+    path              = var.root_disk_attachment
+    device            = local.root_disk_device
+    fstype            = var.disk_fs_type
+    InstanceId        = aws_instance.chips_training_db_01[0].id
+    ImageId           = data.aws_ami.chips_training_ami.id
+    InstanceType      = var.instance_type
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "chips_training_db_01_root_disk_space_warn" {
+  alarm_name          = "${upper(var.environment)} - WARNING - chips-training-01-root-disk-space"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  metric_name         = "disk_used_percent"
+  namespace           = "CWAgent"
+  period              = "300"
+  evaluation_periods  = "1"
+  statistic           = "Average"
+  threshold           = "80"
+  alarm_description   = "The disk space average precentage is over 80% for the last 10 minutes"
+  alarm_actions       = [aws_sns_topic.chips_training_db_01.arn]
+  ok_actions          = [aws_sns_topic.chips_training_db_01.arn]
+  dimensions = {
+    path              = var.root_disk_attachment
+    device            = local.root_disk_device
+    fstype            = var.disk_fs_type
+    InstanceId        = aws_instance.chips_training_db_01[0].id
+    ImageId           = data.aws_ami.chips_training_ami.id
+    InstanceType      = var.instance_type
+  }
+}
+
+# --------------------------------------------------------------------------------------
+resource "aws_cloudwatch_metric_alarm" "chips_training_db_01_u01_space_crit" {
+  alarm_name          = "${upper(var.environment)} - CRITICAL - chips-training-01-u01-disk-space"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  metric_name         = "disk_used_percent"
+  namespace           = "CWAgent"
   period              = "600"
   evaluation_periods  = "1"
   statistic           = "Average"
   threshold           = "90"
-  alarm_description   = "The disk space average precetage is over 90% for the last 10 minutes"
+  alarm_description   = "The disk space average precentage is over 90% for the last 10 minutes"
   alarm_actions       = [aws_sns_topic.chips_training_db_01.arn]
   ok_actions          = [aws_sns_topic.chips_training_db_01.arn]
   dimensions = {
-    path         = "*"
+    path              = local.ebs_info.ebs_u01.path
+    device            = local.ebs_info.ebs_u01.device
+    fstype            = var.disk_fs_type
+    InstanceId        = aws_instance.chips_training_db_01[0].id
+    ImageId           = data.aws_ami.chips_training_ami.id
+    InstanceType      = var.instance_type
   }
 }
 
-resource "aws_cloudwatch_metric_alarm" "chips_training_db_01_server_root_disk_space" {
-  alarm_name          = "WARNING-chips-training-01-root-disk-space"
+resource "aws_cloudwatch_metric_alarm" "chips_training_db_01_u01_space_warn" {
+  alarm_name          = "${upper(var.environment)} - WARNING - chips-training-01-u01-disk-space"
   comparison_operator = "GreaterThanOrEqualToThreshold"
   metric_name         = "disk_used_percent"
-  namespace           = "CHIPS-TRAINING-01/EC2"
+  namespace           = "CWAgent"
   period              = "600"
   evaluation_periods  = "1"
   statistic           = "Average"
   threshold           = "80"
-  alarm_description   = "The disk space average precetage is over 80% for the last 10 minutes"
+  alarm_description   = "The disk space average precentage is over 90% for the last 10 minutes"
   alarm_actions       = [aws_sns_topic.chips_training_db_01.arn]
   ok_actions          = [aws_sns_topic.chips_training_db_01.arn]
   dimensions = {
-    path         = "/"
+    path              = local.ebs_info.ebs_u01.path
+    device            = local.ebs_info.ebs_u01.device
+    fstype            = var.disk_fs_type
+    InstanceId        = aws_instance.chips_training_db_01[0].id
+    ImageId           = data.aws_ami.chips_training_ami.id
+    InstanceType      = var.instance_type
   }
 }
+
+# --------------------------------------------------------------------------------------
+resource "aws_cloudwatch_metric_alarm" "chips_training_db_u02_disk_space_crit" {
+  alarm_name          = "${upper(var.environment)} - CRITICAL - chips-training-01-u02-disk-space"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  metric_name         = "disk_used_percent"
+  namespace           = "CWAgent"
+  period              = "600"
+  evaluation_periods  = "1"
+  statistic           = "Average"
+  threshold           = "90"
+  alarm_description   = "The disk space average precentage is over 90% for the last 10 minutes"
+  alarm_actions       = [aws_sns_topic.chips_training_db_01.arn]
+  ok_actions          = [aws_sns_topic.chips_training_db_01.arn]
+  dimensions = {
+    path              = local.ebs_info.ebs_u01.path
+    device            = local.ebs_info.ebs_u01.device
+    fstype            = var.disk_fs_type
+    InstanceId        = aws_instance.chips_training_db_01[0].id
+    ImageId           = data.aws_ami.chips_training_ami.id
+    InstanceType      = var.instance_type
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "chips_training_db_01_u02_disk_space_warn" {
+  alarm_name          = "${upper(var.environment)} - WARNING - chips-training-01-u02-disk-space"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  metric_name         = "disk_used_percent"
+  namespace           = "CWAgent"
+  period              = "600"
+  evaluation_periods  = "1"
+  statistic           = "Average"
+  threshold           = "80"
+  alarm_description   = "The disk space average precentage is over 90% for the last 10 minutes"
+  alarm_actions       = [aws_sns_topic.chips_training_db_01.arn]
+  ok_actions          = [aws_sns_topic.chips_training_db_01.arn]
+  dimensions = {
+    path              = local.ebs_info.ebs_u02.path
+    device            = local.ebs_info.ebs_u02.device
+    fstype            = var.disk_fs_type
+    InstanceId        = aws_instance.chips_training_db_01[0].id
+    ImageId           = data.aws_ami.chips_training_ami.id
+    InstanceType      = var.instance_type
+  }
+}
+
